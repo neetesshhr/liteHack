e2b3c4c7fed13d60d0501bbbe61081471e85bff0
root <root@ravi.mshome.net>
2020-02-08 10:41:23 +0545

    Heading added

--- header.php
+++ header.php
@@ -16,7 +16,11 @@
           <li><a href="#team">Team</a></li>
           <li class="menu-has-children"><a href="">Donate or Take&give</a>
             <ul>
-              <li><a href="#">Donate</a></li>
+              <!-- Button trigger modal -->
+              <button class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+                Donate
+                </button>
+              
               <li><a href="#">Take and give</a></li>
              
             </ul>
@@ -26,3 +30,22 @@
       </nav><!-- #nav-menu-container -->
     </div>
   </header><!-- #header -->
+
+  <!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
